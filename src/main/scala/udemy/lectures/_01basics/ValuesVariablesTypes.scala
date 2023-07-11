4c38777780acdd9f858fcbe9cc73e664bea86d71
thekodu <praveenrao116@gmail.com>
2023-07-11 16:09:26 +0530

    learnt about variables , valules and types in Scala

---
+++ src/main/scala/udemy/lectures/_01basics/ValuesVariablesTypes.scala
@@ -0,0 +1,28 @@
+package udemy.lectures._01basics
+
+object ValuesVariablesTypes extends App {
+
+  val x = 36 //VALS are immutable
+  println(x)
+
+  val aString: String = "name"
+  val name = "yahoo"
+  println(aString+" "+name)
+
+  val aBoolean: Boolean = true
+  println(aBoolean)
+
+  val aChar: Char = 'a'
+  println(aChar)
+
+  val aShort:Short = 23422;
+  println(aShort)
+
+  val aLong:Long = 3453453453453454534L
+  println(aLong)
+
+  val aFloat:Float = 12.12f
+
+  val aDouble:Double =123.123
+
+}
---
+++ src/main/scala/udemy/playground/Playground.scala
@@ -0,0 +1,9 @@
+package udemy.playground
+
+object Playground {
+
+  def main(args: Array[String]) = {
+      println("I'm ready to learn Scala")
+  }
+
+}
